f6766dfc0ae734aefca232a2e076731d57a1c91a
Aaron Haas <mrhaasguy@gmail.com>
2022-03-12 19:52:45 -0500

    styling wasnt working in all email clients, hooked up juice to convert to inline styling

--- server/emailGenerator.ts
+++ server/emailGenerator.ts
@@ -2,9 +2,11 @@ import { parseFullName } from "parse-full-name";
 import { IDalService } from "./interfaces/types";
 import { takeRandom } from "./utils";
 import { Aes256 } from "./Aes256";
+import fs from "fs";
 import React from "react";
 import ReactDOMServer from "react-dom/server";
 import DailyPrayerEmail from "./templates/daily-prayers-email";
+import juice from "juice";
 
 export async function generateDailyPrayersTemplate(
   userEmail: { userId: string; fullName: string },
@@ -24,7 +26,6 @@ export async function generateDailyPrayersTemplate(
       .filter((p) => !!p)
       .join(",")
   );
-
   const emailElement = React.createElement(DailyPrayerEmail, {
     user: {
       name: parseFullName(userEmail.fullName).first ?? userEmail.fullName,
@@ -34,5 +35,11 @@ export async function generateDailyPrayersTemplate(
     prayedForUrl: `https://prayer-pal.herokuapp.com/api/v1/prayer-request/prayed?userId=${userEmail.userId}&prayerRequestIds=${encryptedPrayerRequestIds}`,
     getMoreUrl: `https://prayer-pal.herokuapp.com/api/v1/prayer-request/daily?userId=${userEmail.userId}`,
   });
-  return ReactDOMServer.renderToStaticMarkup(emailElement);
+  var reactHtml = juice(ReactDOMServer.renderToStaticMarkup(emailElement));
+
+  let html = fs
+    .readFileSync("./server/templates/email-template-base.html", "utf8")
+    .toString();
+
+  return html.replace("{{BODY}}", reactHtml);
 }
